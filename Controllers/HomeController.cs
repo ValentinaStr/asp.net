08e19b8dc3d1fe5fa139660a743867d01a0d5594
Valiantsina <valiantsina_strazhnikava@epam.com>
2024-06-03 18:01:29 +0300

    Create  PartyInvites app

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,27 @@
-﻿using FirstProject.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstProject.Models;
 
 namespace FirstProject.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            // TODO: store response from guest
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
         }
     }
 }
---
+++ Models/Repository.cs
@@ -0,0 +1,13 @@
+﻿namespace FirstProject.Models
+{
+    public static class Repository
+    {
+        private static List<GuestResponse> responses = new();
+        public static IEnumerable<GuestResponse> Responses => responses;
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            responses.Add(response);
+        }
+    }
+}
